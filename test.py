664b933fbc85133f691309a38d86fe593884f7cf
glenn <glenn@69a33542-8223-0410-9a10-e203e7bd99c3>
2007-02-15 23:17:18 +0000

    Updated MM
    git-svn-id: http://cradle.dyndns.org/svn/code@23 69a33542-8223-0410-9a10-e203e7bd99c3

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ class GameWorld():
     def __init__(self):
         self.world = ode.World()
         self.world.setGravity((0,-9.81,0))
+        self.world.setCFM(0.00001)
+        self.world.setERP(0.2)
         self.space = ode.Space()
         self.contactgroup = ode.JointGroup()
         self.objects = []
@@ -11,7 +13,12 @@ class GameWorld():
     def go(self, steps = 1, stepSize = 0.01):
         for i in range(steps):
             self.space.collide('', self.collision_callback)
+            
+            for object in self.objects:
+                object.preStep()
+                
             self.world.step(stepSize)
+            
             for object in self.objects:
                 object.postStep()
                 
@@ -20,12 +27,12 @@ class GameWorld():
     def collision_callback(self, args, geom1, geom2):
         contacts = ode.collide(geom1, geom2)
 
-        for c in contacts:
-            c.setBounce(0.4)
-            c.setBounceVel(15.0)
-            c.setMu(100.0)
-            j = ode.ContactJoint(self.world, self.contactgroup, c)
-            j.attach(geom1.getBody(), geom2.getBody())
+        for contact in contacts:
+            contact.setBounce(0.3)
+            contact.setBounceVel(15.0)
+            contact.setMu(3000)
+            joint = ode.ContactJoint(self.world, self.contactgroup, contact)
+            joint.attach(geom1.getBody(), geom2.getBody())
 
 class StaticObject():
     def __init__(self, gameworld, size = (1.0, 1.0, 1.0)):
@@ -34,11 +41,20 @@ class StaticObject():
     def __str__(self):
         return "P=(%2.2f, %2.2f, %2.2f)" % self.geometry.getPosition()
 
+    def preStep(self):
+        pass # Todo, store in seperate list to dynamics
+
     def postStep(self):
         pass # Todo, store in seperate list to dynamics
 
+    def collidedWith(self, otherObject):
+        pass # Do nothing special
+
 
 class DynamicObject(StaticObject):
+    maxMoveForce = 50000
+    maxMoveVelocity = 10
+    
     def __init__(self, gameworld, size = (1.0,1.0,1.0), weight = 50):
         StaticObject.__init__(self, gameworld, size)
         
@@ -53,8 +69,8 @@ class DynamicObject(StaticObject):
 
     def postStep(self):
         self.alignToZAxis()
-        self.motor.setXParam(ode.ParamVel, 0)
-        self.motor.setYParam(ode.ParamVel, 0)
+        self.motor.setXParam(ode.ParamFMax, 0)
+        self.motor.setYParam(ode.ParamFMax, 0)
 
     def alignToZAxis(self):
         rot = self.body.getAngularVel()
@@ -63,10 +79,15 @@ class DynamicObject(StaticObject):
         self.body.setQuaternion((old_quat[0] / quat_len, 0, 0, old_quat[3] / quat_len))
         self.body.setAngularVel((0,0,rot[2]))
         # http://opende.sourceforge.net/wiki/index.php/HOWTO_constrain_objects_to_2d
+        # self.body.setLinearVel((self.body.getLinearVel()[0], self.body.getLinearVel()[1], 0.0))
         
     def moveLeft(self):
-        self.motor.setXParam(ode.ParamVel, -10)
-        self.motor.setXParam(ode.ParamFMax, 50000)
+        self.motor.setXParam(ode.ParamVel, -DynamicObject.maxMoveVelocity)
+        self.motor.setXParam(ode.ParamFMax, DynamicObject.maxMoveForce)
+        
+    def moveRight(self):
+        self.motor.setXParam(ode.ParamVel,  DynamicObject.maxMoveVelocity)
+        self.motor.setXParam(ode.ParamFMax, DynamicObject.maxMoveForce)
 
     def __str__(self):
         return StaticObject.__str__(self) + ", LV=(%2.2f, %2.2f, %2.2f), AV=(%2.2f, %2.2f, %2.2f)" % \
@@ -82,6 +103,7 @@ class TestGame():
     def go(self):
         tests = [method for method in dir(self) if method.startswith('test_')]
         for test in tests:
+            print "Running", test
             self.setup()
             eval(("self.%s()" % test))
             print ".",
@@ -106,7 +128,7 @@ class TestGame():
             if dynamic.body.getLinearVel()[1] > 0.0:
                 on_way_up = True
 
-            if on_way_up and dynamic.body.getLinearVel()[1] < 0.0:
+            if on_way_up and dynamic.body.getLinearVel()[1] <= 0.0:
                 on_way_down_second_time = True
 
             world.go()
@@ -128,21 +150,55 @@ class TestGame():
         assert_equal( 0.0, dynamic.body.getLinearVel()[1] )
 
     def test_dynamic_rebounding_off_static(self):
-        assert_equal( 15.0, round(self.get_maximum_rebound_height(100),0))
-        assert_equal( 8.0, round(self.get_maximum_rebound_height(50),0))
-        assert_equal( 4.0, round(self.get_maximum_rebound_height(20),0))
+        assert_equal( 9.0, round(self.get_maximum_rebound_height(100),0))
+        assert_equal( 5.0, round(self.get_maximum_rebound_height(50),0))
+        assert_equal( 2.0, round(self.get_maximum_rebound_height(20),0))
         assert_equal( 1.0, round(self.get_maximum_rebound_height(10),0))
 
     def test_does_not_move_on_z_axis(self):
+        static = StaticObject(world, size=(1000.0, 1.0, 1.0))
         for i in range(200):
+            dynamic.moveRight()
             world.go()
-            assert_equal( 0.0, dynamic.body.getPosition()[0] )
             assert_equal( 0.0, dynamic.body.getPosition()[2] )
 
     def test_move_left(self):
         static = StaticObject(world, size=(100.0, 100.0, 0.0))
         dynamic.body.setPosition((0.0, 1.0, 0.0))
-        dynamic.moveLeft()
+        for i in range(100):
+            dynamic.moveLeft()
+            world.go()
+            
+        assert_equal( -10.0, round(dynamic.body.getPosition()[0],0))
+
+    def test_move_right(self):
+        static = StaticObject(world, size=(1000.0, 1.0, 1.0))
+        dynamic.body.setPosition((0.0, 1.0, 0.0))
+        for i in range(100):
+            dynamic.moveRight()
+            world.go()
+            
+        assert_equal( 10.0, round(dynamic.body.getPosition()[0],0))
+
+    def test_slide_after_move(self):
+        static = StaticObject(world, size=(1000.0, 1.0, 1.0))
+        dynamic.body.setPosition((0.0, 1.0, 0.0))
+        i = 0
+        for i in range(100):
+            dynamic.moveRight()
+            world.go()
+            print dynamic
+            
+        while (round(dynamic.body.getLinearVel()[1],2), \
+               round(dynamic.body.getLinearVel()[2],2), \
+               round(dynamic.body.getLinearVel()[0],2)) != (0,0,0):
+            world.go()
+            i+=1
+            print i, dynamic
+        
+        assert_equal( 0.0, round(dynamic.body.getPosition()[2],2))
+        assert_equal( 1.0, round(dynamic.body.getPosition()[1],2))
+        assert_equal( 10.0, round(dynamic.body.getPosition()[0],2))
     
 t = TestGame()
 t.go()
