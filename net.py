8f0dfebfb163e110a45c3de27da10a67076b29e3
glenn <glenn@69a33542-8223-0410-9a10-e203e7bd99c3>
2007-02-17 19:24:05 +0000

    Much update net code. All the code is pretty much there for making ingame chat. Not sure how I'm going to implement updating game states.
    git-svn-id: http://cradle.dyndns.org/svn/code@36 69a33542-8223-0410-9a10-e203e7bd99c3

--- net.py
+++ net.py
@@ -3,97 +3,115 @@ from twisted.words.xish import domish, xmlstream
 from twisted.internet import reactor
 import time
 
-name = 'cradle'
-server='cradle.dyndns.org'
-resource = 'AssaultVector'
-password = 'ug0/*worz'
-me = '%s@%s/%s' % (name, server, resource)
+class NetCode:
+	def __init__(self, name, server, resource, password):
+		self.reactor = reactor
+		self.thexmlstream = None
+		self.tryandregister = 1
+		self.messageNum = 0
 
-thexmlstream = None
-tryandregister = 1
+		me = u'%s@%s/%s' % (name, server, resource)
+		myJid = jid.JID(me)
+		self.factory = client.basicClientFactory(myJid,password)
 
-def initOnline(xmlstream):
-     global factory
-     print 'Initializing...'
-     xmlstream.addObserver('/message', gotMessage)
-     xmlstream.addObserver('/presence', gotPresence)
-     xmlstream.addObserver('/iq', gotIq)
-     xmlstream.addObserver('/*', gotSomething)
+		# Register authentication callbacks
+		self.factory.addBootstrap('//event/stream/authd', self.authd)
+		self.factory.addBootstrap('//event/client/basicauth/invaliduser', self.invaliduserEvent)
+		self.factory.addBootstrap('//event/client/basicauth/authfailed', self.authfailedEvent)
+		self.factory.addBootstrap('//event/client/basicauth/registerfailed', self.registerfailedEvent)
 
-def authd(xmlstream):
-     thexmlstream = xmlstream
-     print "we've authd!"
-     print repr(xmlstream)
+		# Go!
+		self.reactor.connectTCP(server, 5222, self.factory)
+		self.reactor.startRunning()
+		self.update()
+		#self.reactor.run()
 
-     #need to send presence so clients know we're
-     #actually online
-     presence = domish.Element(('jabber:client', 'presence'))
-     presence.addElement('status').addContent('Online')
-     xmlstream.send(presence)
+	def sendMessage(self, text):
+		message = domish.Element((None, 'message'))
+		message['to'] = 'cradle@gmail.com'
+		message['id'] = "%i" % self.messageNum
+		message['type'] = 'chat'
+		self.messageNum += 1
+		message.addElement('body', content='Hi!')
+		event = domish.Element(('jabber:x:event', 'x'))
+		event.addElement("offline")
+		event.addElement("delivered")
+		event.addElement("composing")
+		message.addChild(event)
+		if(self.thexmlstream):
+		    self.thexmlstream.send(message)
+		    return True
+		else:
+		    return False
+		 
+	def update(self):
+		self.reactor.runUntilCurrent()
+		self.reactor.doIteration(0)
 
-     initOnline(xmlstream)
+	def authd(self, xmlstream):
+		self.thexmlstream = xmlstream
+		print "we've authd!"
+		print repr(xmlstream)
 
-def gotMessage(el):
-     print 'Got message: %s' % str(el.attributes)
+		#need to send presence so clients know we're
+		#actually online
+		presence = domish.Element(('jabber:client', 'presence'))
+		presence.addElement('status').addContent('Online')
+		xmlstream.send(presence)
 
-def gotSomething(el):
-     print 'Got something: %s -> %s' % (el.name, str(el.attributes))
+		print 'Initializing...'
+		xmlstream.addObserver('/message', self.gotMessage)
+		xmlstream.addObserver('/presence', self.gotPresence)
+		xmlstream.addObserver('/iq', self.gotIq)
+		xmlstream.addObserver('/*', self.gotSomething)
+		print 'Done init'
 
-def gotIq(el):
-     print 'Got IQ: %s' % str(el.attributes)
+	def gotMessage(self, el):
+		print 'Got message'
 
-def gotPresence(el):
-     print 'We got a presence message!'
-     print repr(el.attributes)
-     try:
-         t = el.attributes['type']
-         if t == 'subscribe':
-             # Grant every subscription request
-             xmlstream.send(domish.Element(('jabber:client', 'presence'), attribs={
-                 'from': me,
-                 'to':el.attributes['from'],
-                 'type':'subscribed'
-             }))
-     except KeyError:
-         # Big fat ignore
-         pass
+	def gotSomething(self, el):
+		print el.toXml()
 
-def invaliduserEvent(xmlstream):
-     print 'Invalid user!'
-     global tryandregister
-     if tryandregister:
-         tryandregister = 0
-         print 'Attempting to register...'
-         global factory
-         factory.authenticator.registerAccount(name, password)
-     else:
-         global reactor
-         reactor.stop()
+	def gotIq(self, el):
+		print 'Got IQ'
 
-def authfailedEvent(xmlstream):
-     global reactor
-     print 'Auth failed!'
-     reactor.stop()
+	def gotPresence(self, el):
+		print 'We got a presence message'
+		try:
+			t = el.attributes['type']
+			if t == 'subscribe':
+				# Grant every subscription request
+				xmlstream.send(domish.Element(('jabber:client', 'presence'), attribs={
+					'from': me,
+					'to':el.attributes['from'],
+					'type':'subscribed'
+				}))
+		except KeyError:
+			# Big fat ignore
+			pass
 
-def registerfailedEvent(xmlstream):
-     global reactor
-     print 'Register failed!'
-     reactor.stop()
+	def invaliduserEvent(self, xmlstream):
+		print 'Invalid user!'
+		if self.tryandregister:
+			self.tryandregister = 0
+			print 'Attempting to register...'
+			self.factory.authenticator.registerAccount(name, password)
+		else:
+			'Registration failed. Stopping reactor'
+			self.reactor.stop()
 
-myJid = jid.JID(me)
-secret = password
-factory = client.basicClientFactory(myJid,secret)
+	def authfailedEvent(self, xmlstream):
+		print 'Auth failed!'
+		self.reactor.stop()
 
-# Register authentication callbacks
-factory.addBootstrap('//event/stream/authd', authd)
-factory.addBootstrap('//event/client/basicauth/invaliduser', invaliduserEvent)
-factory.addBootstrap('//event/client/basicauth/authfailed', authfailedEvent)
-factory.addBootstrap('//event/client/basicauth/registerfailed', registerfailedEvent)
+	def registerfailedEvent(self, xmlstream):
+		print 'Register failed!'
+		self.reactor.stop()
 
-# Go!
-reactor.connectTCP(server, 5222, factory)
-reactor.startRunning()
-while reactor.running:
-    reactor.runUntilCurrent()
-    reactor.doIteration(0)
-    time.sleep(1)
+if __name__ == "__main__":
+	n = NetCode("cradle", "cradle.dyndns.org", "AssaultVector", "ug0/*worz")
+
+	while 1:
+		time.sleep(0.1)
+		n.update()
+		#n.sendMessage("test")
