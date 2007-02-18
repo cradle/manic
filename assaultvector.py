7cf4964df8e687ca2bd25a1b045c7e0b7a82c957
glenn <glenn@69a33542-8223-0410-9a10-e203e7bd99c3>
2007-02-18 17:01:30 +0000

    Buffered keyboard input does stuff!
    git-svn-id: http://cradle.dyndns.org/svn/code@39 69a33542-8223-0410-9a10-e203e7bd99c3

--- assaultvector.py
+++ assaultvector.py
@@ -2,31 +2,306 @@ import ode, math
 import Ogre as ogre
 import CEGUI as CEGUI
 import OIS
-from Ogre.sf_OIS import *
-#from CEGUI_framework import *   ## we need the OIS version of the framelistener
 
 def cegui_reldim ( x ) :
     return CEGUI.UDim((x),0)
 
-class GameFrameListener ( FrameListener ):
-    def __init__( self, game, renderWindow, camera ):
-        FrameListener.__init__(self, renderWindow, camera)
-        self.game = game
+def getPluginPath():
+    """Return the absolute path to a valid plugins.cfg file.""" 
+    import sys
+    import os
+    import os.path
+    
+    paths = [os.path.join(os.getcwd(), 'plugins.cfg'),
+             '/etc/OGRE/plugins.cfg',
+             os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                              'plugins.cfg')]
+    for path in paths:
+        if os.path.exists(path):
+            return path
 
-    def frameEnded(self, evt):
-        self.game.frameEnded(evt.timeSinceLastFrame, self.Keyboard, self.Mouse)
-        return FrameListener.frameEnded(self, evt)
+    sys.stderr.write("\n"
+        "** Warning: Unable to locate a suitable plugins.cfg file.\n"
+        "** Warning: Please check your ogre installation and copy a\n"
+        "** Warning: working plugins.cfg file to the current directory.\n\n")
+    raise ogre.Exception(0, "can't locate the 'plugins.cfg' file", "")
+    
+class Application(object):
+    debugText = ""
+    
+    def __init__(self):
+        self.frameListener = None
+        self.root = None
+        self.camera = None
+        self.renderWindow = None
+        self.sceneManager = None
+        self.world = None
+
+    def go(self):
+        "Starts the rendering loop."
+        if not self._setUp():
+            return
+        if self._isPsycoEnabled():
+            self._activatePsyco()
+        self.root.startRendering()
+
+    def _setUp(self):
+        """This sets up the ogre application, and returns false if the user
+        hits "cancel" in the dialog box."""
+        self.root = ogre.Root(getPluginPath())
+        self.root.setFrameSmoothingPeriod (5.0)
+
+        self._setUpResources()
+        if not self._configure():
+            return False
+        
+        self._chooseSceneManager()
+        self._createWorld()
+        self._createCamera()
+        self._createViewports()
+
+        ogre.TextureManager.getSingleton().setDefaultNumMipmaps (5)
+
+        self._createResourceListener()
+        self._loadResources()
+
+        self._createScene()
+        self._createFrameListener()
+        return True
+
+    def _setUpResources(self):
+        """This sets up Ogre's resources, which are required to be in
+        resources.cfg."""
+        config = ogre.ConfigFile()
+        config.load('resources.cfg' ) #, '', False )
+        seci = config.getSectionIterator()
+        while (seci.hasMoreElements()):
+            secName = seci.peekNextKey()
+            settings = seci.getNext()
+            ## Note that getMultiMapSettings is a Python-Ogre extension to return a multimap in a list of tuples
+            settingslist = config.getMultiMapSettings ( settings )
+            for typeName, archName in settingslist:
+                ogre.ResourceGroupManager.getSingleton().addResourceLocation(archName, typeName, secName)
+                    
+    def _createResourceListener(self):
+        """This method is here if you want to add a resource listener to check
+        the status of resources loading."""
+        pass
+        
+    def _createWorld ( self ):
+        """ this should be overridden when supporting the OgreRefApp framework.  Also note you 
+        will have to override __createCamera"""
+        pass
+
+    def _loadResources(self):
+        """This loads all initial resources.  Redefine this if you do not want
+        to load all resources at startup."""
+        ogre.ResourceGroupManager.getSingleton().initialiseAllResourceGroups()
+
+    def _configure(self):
+        """This shows the config dialog and creates the renderWindow."""
+        carryOn = self.root.showConfigDialog()
+        if carryOn:
+            self.renderWindow = self.root.initialise(True, "Assault Vector")
+        return carryOn
+
+    def _chooseSceneManager(self):
+        """Chooses a default SceneManager."""
+        #typedef uint16 SceneTypeMask;
+        #md=ogre.SceneManagerMetaData()
+        #md.sceneTypeMask=ogre.ST_GENERIC
+        #print dir(self.root)    
+        self.sceneManager = self.root.createSceneManager(ogre.ST_GENERIC,"ExampleSMInstance")
+
+    def _isPsycoEnabled(self):
+        """Override this function and return True to turn on Psyco"""
+        return False
+
+    def _activatePsyco(self):        
+       """Import Psyco if available"""
+       try:
+           import psyco
+           psyco.full()
+       except ImportError:
+           pass
+
+class GameKeyListener(OIS.KeyListener):
+    def __init__(self):
+        OIS.KeyListener.__init__(self)
+
+    def keyPressed(self, evt):
+        print "Key Pressed"
+    
+    def keyReleased(self, evt):
+        print "Key Released"
+
+class FrameListener(ogre.FrameListener, ogre.WindowEventListener):
+    """A default frame listener, which takes care of basic mouse and keyboard
+    input."""
+    def __init__(self, game, renderWindow, camera, bufferedKeys = False, bufferedMouse = True, bufferedJoy = False):
+        ogre.FrameListener.__init__(self)
+        ogre.WindowEventListener.__init__(self)
+        self.game = game
+        self.camera = camera
+        self.renderWindow = renderWindow
+        self.statisticsOn = True
+        self.numScreenShots = 0
+        self.timeUntilNextToggle = 0
+        self.sceneDetailIndex = 0
+        self.moveScale = 0.0
+        self.rotationScale = 0.0
+        self.translateVector = ogre.Vector3(0.0,0.0,0.0)
+        self.filtering = ogre.TFO_BILINEAR
+        self.showDebugOverlay(True)
+        self.moveSpeed = 100.0
+        self.rotationSpeed = 8.0
+        self.displayCameraDetails = False
+        self.bufferedKeys = bufferedKeys
+        self.bufferedMouse = bufferedMouse
+        self.bufferedJoy = bufferedJoy
+        self.MenuMode = False   # lets understand a simple menu function
+
+        self._setupInput()
+
+        self.keylistener = GameKeyListener()
+        self.Keyboard.setEventCallback(self.keylistener)
+        
+    def __del__ (self ):
+      ogre.WindowEventUtilities.removeWindowEventListener(self.renderWindow, self)
+      self.windowClosed(self.renderWindow)
+      
+    def _setupInput(self):
+        # ignore buffered input
+        
+         windowHnd = self.renderWindow.getCustomAttributeInt("WINDOW")
+         self.InputManager = \
+             OIS.createPythonInputSystem([("WINDOW",str(windowHnd))])
+         
+         #Create all devices (We only catch joystick exceptions here, as, most people have Key/Mouse)
+         self.Keyboard = self.InputManager.createInputObjectKeyboard( OIS.OISKeyboard, self.bufferedKeys )
+         self.Mouse = self.InputManager.createInputObjectMouse( OIS.OISMouse, self.bufferedMouse )
+         try :
+            self.Joy = self.InputManager.createInputObjectJoyStick( OIS.OISJoyStick, bufferedJoy )
+         except:
+            self.Joy = False
+         
+         #Set initial mouse clipping size
+         self.windowResized(self.renderWindow)
+         
+         self.showDebugOverlay(True)
+         
+         #Register as a Window listener
+         ogre.WindowEventUtilities.addWindowEventListener(self.renderWindow, self);
+         
+    def setMenuMode(self, mode):
+        self.MenuMode = mode
+        
+    def _UpdateSimulation( self, frameEvent ):
+        # create a real version of this to update the simulation
+        pass 
+           
+    def windowResized (self, rw):
+         [width, height, depth, left, top] = rw.getMetrics()  # Note the wrapped function as default needs unsigned int's
+         ms = self.Mouse.getMouseState()
+         ms.width = width
+         ms.height = height
+         
+    def windowClosed(self, rw):
+      #Only close for window that created OIS (mWindow)
+      if( rw == self.renderWindow ):
+         if( self.InputManager ):
+            self.InputManager.destroyInputObjectMouse( self.Mouse )
+            self.InputManager.destroyInputObjectKeyboard( self.Keyboard )
+            if self.Joy:
+                self.InputManager.destroyInputObjectJoyStick( self.Joy )
+            OIS.InputManager.destroyInputSystem(self.InputManager)
+            self.InputManager=None
+            
+    def frameStarted(self, frameEvent):
+        self.game.frameEnded(frameEvent.timeSinceLastFrame, self.Keyboard, self.Mouse)
+        
+        if(self.renderWindow.isClosed()):
+            return False
+        
+        ##Need to capture/update each device - this will also trigger any listeners
+        self.Keyboard.capture()    
+        self.Mouse.capture()
+        if( self.Joy ):
+            self.Joy.capture()
+
+        return True
+
+    def frameEnded(self, frameEvent):
+        self._updateStatistics()
+        return True
+
+    def showDebugOverlay(self, show):
+        """Turns the debug overlay (frame statistics) on or off."""
+        overlay = ogre.OverlayManager.getSingleton().getByName('Core/DebugOverlay')
+        if overlay is None:
+            raise ogre.Exception(111, "Could not find overlay Core/DebugOverlay", "SampleFramework.py")
+        if show:
+            overlay.show()
+        else:
+            overlay.hide()
+
+    def _processUnbufferedKeyInput(self, frameEvent):
+        if self.Keyboard.isKeyDown(OIS.KC_ESCAPE) or self.Keyboard.isKeyDown(OIS.KC_Q):
+            return False
+        return True        
+        
+    def _isToggleKeyDown(self, keyCode, toggleTime = 1.0):
+        if self.Keyboard.isKeyDown(keyCode)and self.timeUntilNextToggle <=0:
+            self.timeUntilNextToggle = toggleTime
+            return True
+        return False
+        
+    def _isToggleMouseDown(self, Button, toggleTime = 1.0): 
+        ms = self.Mouse.getMouseState() 
+        if ms.buttonDown( Button ) and self.timeUntilNextToggle <=0: 
+            self.timeUntilNextToggle = toggleTime 
+            return True 
+        return False 
+
+    def _processUnbufferedMouseInput(self, frameEvent):
+        ms = self.Mouse.getMouseState()
+        if ms.buttonDown( OIS.MB_Right ):
+            self.translateVector.x += ms.X.rel * 0.13
+            self.translateVector.y -= ms.Y.rel * 0.13
+        else:
+            self.rotationX = ogre.Radian(- ms.X.rel * 0.13)
+            self.rotationY = ogre.Radian(- ms.Y.rel * 0.13)
+
+    def _moveCamera(self):
+        self.camera.yaw(self.rotationX)
+        self.camera.pitch(self.rotationY)
+        try:
+            self.camera.translate(self.translateVector) # for using OgreRefApp
+        except AttributeError:
+            self.camera.moveRelative(self.translateVector)
+
+    def _updateStatistics(self):
+        statistics = self.renderWindow
+        self._setGuiCaption('Core/AverageFps', 'Average FPS: %f' % statistics.getAverageFPS())
+        self._setGuiCaption('Core/CurrFps', 'Current FPS: %f' % statistics.getLastFPS())
+        self._setGuiCaption('Core/BestFps',
+                             'Best FPS: %f %d ms' % (statistics.getBestFPS(), statistics.getBestFrameTime()))
+        self._setGuiCaption('Core/WorstFps',
+                             'Worst FPS: %f %d ms' % (statistics.getWorstFPS(), statistics.getWorstFrameTime()))
+        self._setGuiCaption('Core/NumTris', 'Triangle Count: %d' % statistics.getTriangleCount())
+        self._setGuiCaption('Core/DebugText', Application.debugText)
+
+    def _setGuiCaption(self, elementName, text):
+        element = ogre.OverlayManager.getSingleton().getOverlayElement(elementName, False)
+        element.setCaption(ogre.UTFString(text))
     
 class GameWorld(Application):
-    # /*************************************************************************
-    #     Free function handler called when editbox text changes
-    # *************************************************************************/
     def textChangedHandler(self, e):
         ##find the static box
         st = CEGUI.WindowManager.getSingleton().getWindow("TextWindow/Static")
 
         ## set text from the edit box...
-        st.setText(e.window.getText())
+        st.setText(st.getText() + e.window.getText())
 
         return True
     
@@ -155,7 +430,7 @@ class GameWorld(Application):
         
     def _createFrameListener(self):
         ## note we pass ourselves as the demo to the framelistener
-        self.frameListener = GameFrameListener(self, self.renderWindow, self.camera)
+        self.frameListener = FrameListener(self, self.renderWindow, self.camera, True)
         self.root.addFrameListener(self.frameListener)
         self.frameListener.showDebugOverlay(False)
         
