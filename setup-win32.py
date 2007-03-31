ed252e64a06e91fe15376e4cc1d6673ce15b0782
glenn <glenn@69a33542-8223-0410-9a10-e203e7bd99c3>
2007-03-31 07:07:22 +0000

    Added new icon for executables
    git-svn-id: http://cradle.dyndns.org/svn/code@255 69a33542-8223-0410-9a10-e203e7bd99c3

--- setup-win32.py
+++ setup-win32.py
@@ -1,4 +1,19 @@
 from distutils.core import setup 
 import py2exe
 
-setup(console=["client.py","server.py","listenClient.py"]) 
+setup(
+    console=[
+        {
+            "script" : "client.py",
+            "icon_resources" : [(1, "av.ico")]
+            },
+        {
+            "script" : "server.py",
+            "icon_resources" : [(1, "av.ico")]
+            },
+        {
+            "script" : "listenClient.py",
+            "icon_resources" : [(1, "av.ico")]
+            }
+        ]
+    ) 
