73513bb90d31fa223d7586b2ac362690ad72a0df
glenn <glenn@69a33542-8223-0410-9a10-e203e7bd99c3>
2008-05-18 18:26:18 +0000

    Refactoring Network
    git-svn-id: http://cradle.dyndns.org/svn/code@315 69a33542-8223-0410-9a10-e203e7bd99c3

---
+++ net/client.py
@@ -0,0 +1,86 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+from twisted.spread import banana
+from collections import deque
+import zlib
+import time
+
+class ping(object):
+    def __init__(self, number, time):
+        self.number = number
+        self.time = time
+
+class NetworkClient(DatagramProtocol):
+    def __init__(self, host = "127.0.0.1", port = 10001):
+        self._messages = deque()
+        self.reactor = reactor
+        self.serverIP = None
+        self.port = port
+        self.reactor.startRunning()
+        self.reactor.resolve(host).addCallback(self.gotIP)
+        self._pingSendTime = time.time()
+        self.roundTripTime = 0.0
+        self.serverOffset = 0.0
+        self.lastPingNumber = 0
+        self.pings = []
+        self.pingNumber = 0
+        self.timeBetweenPings = 0.5
+        self.timeUntilNextPing = 0.0
+        self.debugSendPacketLength = 0
+        self.debugReceivePacketLength = 0
+
+    def gotIP(self, ip):
+        print "Got IP", ip
+        self.serverIP = ip
+        self.reactor.listenUDP(0, self)
+
+    def ping(self):
+        if self.serverIP != None:
+            self.pingNumber += 1
+            self.send(["p", self.pingNumber, int(self.roundTripTime*100)])
+            self.pings.append(ping(self.pingNumber, time.time()))
+        
+    def startProtocol(self):
+        self.transport.connect(self.serverIP, self.port)
+        
+    def datagramReceived(self, data, (host, port)):
+        self.debugReceivePacketLength = len(data)
+        message = banana.decode(zlib.decompress(data))
+        if type(message[0]) == str and message[0] == "p":
+            for ping in self.pings:
+                if ping.number == message[1]:
+                    self.roundTripTime = time.time() - ping.time;
+                    self.serverOffset = time.time() - (message[2] + self.roundTripTime/2)
+                    self.pings = [p for p in self.pings if p.number <= ping.number]
+        else:
+            self._messages.append(message)
+
+    # Possibly invoked if there is no server listening on the
+    # address to which we are sending.
+    def connectionRefused(self):
+        print "No Server"
+
+    def send(self, obj):
+        data = zlib.compress(banana.encode(obj),1)
+        self.transport.write(data)
+        self.debugSendPacketLength = len(data)
+
+    def update(self, elapsedTime):
+        if self.serverIP != None:
+            self.timeUntilNextPing -= elapsedTime
+            if self.timeUntilNextPing <= 0.0:
+                self.ping()
+                self.timeUntilNextPing = self.timeBetweenPings
+                
+        self.reactor.runUntilCurrent()
+        self.reactor.doIteration(0)
+
+    def clearMessages(self):
+        self._messages.clear()
+
+if __name__ == "__main__":
+    client = NetworkClient()
+    while(reactor.running):
+        client.update(0.1)
+        time.sleep(0.1)
+    
---
+++ net/server.py
@@ -0,0 +1,99 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+from twisted.spread import banana
+import time
+import zlib
+
+class Client():
+    def __init__(self, address, transport):
+        self.address = address
+        self.messages = []
+        self.transport = transport
+        self.lastMessageTime = time.time()
+        self.timeout = 10.0
+        self.ping = 0
+        self.player = None
+        self.packetNumber = 0
+
+    def __str__(self):
+        return "%s:%i" % self.address
+
+    def debug(self):
+        output = ""
+        output += "%s:%i " % self.address
+        for message in self.messages:
+            output += "    %s\n" % message
+        output += "ping(%s) " % self.ping
+        output += "#(%s)" % self.packetNumber
+        return output
+
+    def __eq__(self, other):
+        return self.address == other.address
+
+    def timedOut(self):
+        return (time.time() - self.lastMessageTime) > self.timeout
+
+    def push(self, data):
+        if type(data) == list and len(data) > 0 and data[0] == "p":
+            self.send(["p", data[1], time.time()])
+            self.ping = data[2]
+            if self.player:
+                self.player.ping = data[2]
+        else:
+            self.messages.insert(0,data)
+            
+        self.lastMessageTime = time.time()
+
+    def hasMoreMessages(self):
+        return len(self.messages) != 0
+
+    def pop(self):
+        return self.messages.pop()
+
+    def send(self, data):
+        toSend = zlib.compress(banana.encode(data),4)
+        self.transport.write(toSend, self.address)
+        NetworkServer.debugSendPacketLength = len(toSend)
+    
+class NetworkServer(DatagramProtocol):
+    debugSendPacketLength = 0
+    
+    def __init__(self, connectedCallback, port = 10001, debug = False):
+        self.clients = []
+        self.debug = debug
+        self.reactor = reactor
+        self.reactor.startRunning()
+        self.reactor.listenUDP(port, self)
+        self.connectedCallback = connectedCallback
+        self.debugSendPacketLength = 0
+        self.debugReceivePacketLength = 0
+        if self.debug: print "Started"
+        
+    def datagramReceived(self, data, address):
+        if self.debug: print "Received Packet",
+        self.debugReceivePacketLength = len(data)
+        # Allocate the received datagram to the correct client
+        client = Client(address, self.transport)
+        if client not in self.clients:
+            if self.debug: print "!New!",
+            self.clients.append(client)
+            self.connectedCallback(client)
+        else:
+            client = self.clients[self.clients.index(client)]
+
+        if self.debug: print "Data", client.debug()
+        client.push(banana.decode(zlib.decompress(data)))
+
+    def update(self, time = 0):
+        self.debugSendPacketLength = NetworkServer.debugSendPacketLength
+        self.reactor.runUntilCurrent()
+        self.reactor.doIteration(0)
+
+if __name__ == "__main__":
+    def testCallback(client):
+        print client
+    
+    server = NetworkServer(testCallback, debug = True)
+    while(reactor.running):
+        server.update(0.1)
+        time.sleep(0.1)
